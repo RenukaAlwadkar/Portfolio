04dc43a565b805ac651e722c4cdbc0eefd8afa19
rushiborkar <rushiborkar2005@gmail.com>
2025-04-24 21:55:02 +0530

    added project2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const path=require("path")
 const port = 3000
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"))
 
 app.get('/', (req, res) => {
   let siteName = "Addidas"
